b3eda8b0915e537d040c844e3f4f5b1c57a95023
danneg <dana@boxalino.com>
2020-09-30 19:14:58 +0200

    [component][customers] attributes mapping

--- Service/Component/Customer.php
+++ Service/Component/Customer.php
@@ -49,10 +49,7 @@ class Customer extends Base
         }
 
         $this->getLogger()->info("Boxalino Exporter: CUSTOMERS EXPORT for account: $this->account");
-        $count = CustomerExporterInterface::PAGINATION;
-        $page = 1;
-        $header = true;
-        $attrsFromDb = ['int'=>[], 'static'=>[], 'varchar'=>[], 'datetime'=>[]];
+        $page = 1; $header = true; $attrsFromDb = ['int'=>[], 'static'=>[], 'varchar'=>[], 'datetime'=>[]];
 
         $this->getLogger()->info("Boxalino Exporter: get final customer attributes for account: $this->account" );
         $customerAttributes = $this->getAttributes();
@@ -61,65 +58,57 @@ class Customer extends Base
         $result = $this->exporterResource->getAttributesByCodes($customerAttributes);
         foreach ($result as $attr)
         {
-            if (isset($attrsFromDb[$attr['backend_type']])) {
+            if (isset($attrsFromDb[$attr['backend_type']]))
+            {
                 $attrsFromDb[$attr['backend_type']][] = $attr['backend_type'] == 'static' ? $attr['attribute_code'] : $attr['aid'];
             }
         }
-
         $fieldsForCustomerSelect =  array_merge(['entity_id', 'confirmation'], $attrsFromDb['static']);
         do {
             $this->getLogger()->info("Boxalino Exporter: Customers - load page $page for account: $this->account");
             $this->getLogger()->info("Boxalino Exporter: Customers - get customer ids for page $page for account:  $this->account");
 
-            $customers_to_save = [];
             $customers = $this->exporterResource->getAddressByFieldsAndLimit(CustomerExporterInterface::PAGINATION, $page, $fieldsForCustomerSelect);
-            $customerAttributesValues = $this->exporterResource->getUnionAttributesByAttributesAndIds($attrsFromDb, array_column($customers, 'entity_id'));
+            $customerIds = array_column($customers, 'entity_id');  $saveList = [];
+            $customerAttributesValues = $this->exporterResource->getUnionAttributesByAttributesAndIds($attrsFromDb, $customerIds);
             if(!empty($customerAttributesValues))
             {
                 $this->getLogger()->info("Boxalino Exporter: Customers - retrieve data for side queries page $page for account: $this->account");
                 foreach ($customerAttributesValues as $r)
                 {
-                    $customers[$r['entity_id']][$r['attribute_code']] = $r['value'];
+                    $customers[array_search($r['entity_id'], $customerIds)][$r['attribute_code']] = $r['value'];
                 }
             }
-
             $this->getLogger()->info("Boxalino Exporter: CUSTOMERS EXPORT - load data per customer for page #$page for account:  $this->account");
-            foreach ($customers as $customer)
+            foreach ($customers as $row)
             {
-                $id = $customer['entity_id'];
-                $countryCode = $customer['country_id'];
-                if (array_key_exists('gender', $customer)) {
-                    if ($customer['gender'] % 2 == 0)
-                    {
-                        $customer['gender'] = 'female';
-                    } else {
-                        $customer['gender'] = 'male';
-                    }
+                $countryCode = $row['country_id'];
+                if (array_key_exists('gender', $row)) {
+                    $row['gender'] = is_null($row['gender']) ? null : ($row['gender'] % 2 == 0 ? 'female' : 'male');
                 }
-                $customer_to_save = [
-                    'customer_id' => $id,
-                    'country' => !empty($countryCode) ? $this->countryHelper->loadByCode($countryCode)->getName() : '',
-                    'zip' => array_key_exists('postcode', $customer) ? $customer['postcode'] : '',
+
+                $customer = [
+                    'customer_id' => $row['entity_id'],
+                    'country' => empty($countryCode) ? null : $this->countryHelper->loadByCode($countryCode)->getName(),
+                    'zip' => $row['postcode']
                 ];
-                foreach($customerAttributes as $attr) {
-                    $customer_to_save[$attr] = array_key_exists($attr, $customer) ? $customer[$attr] : '';
+                foreach($customerAttributes as $attr)
+                {
+                    $customer[$attr] = array_key_exists($attr, $row) ? $row[$attr] : '';
                 }
-                $customers_to_save[] = $customer_to_save;
-            }
-            $data = $customers_to_save;
-
-            if (count($customers) == 0 && $header) {
-                return;
+                $saveList[] = $customer;
             }
 
+            $data = $saveList;
+            if (count($customers) == 0 && $header) { return; }
             if ($header)
             {
-                $data = array_merge(array(array_keys(end($customers_to_save))), $customers_to_save);
+                $data = array_merge(array(array_keys(end($saveList))), $saveList);
                 $header = false;
             }
             $this->getLogger()->info("Boxalino Exporter: CUSTOMERS EXPORT - save to file for page $page for account: $this->account");
             $this->getFiles()->savePartToCsv("customers.csv", $data);
-            $data = null; $count = count($customers_to_save); $page++;
+            $data = null; $count = count($saveList); $page++;
 
         } while ($count >= CustomerExporterInterface::PAGINATION);
 
